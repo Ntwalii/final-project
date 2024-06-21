7c70b1d8a908ad6d39db2dfd0f6f04647ac6a3de
cyusakeny <gasananike@gmail.com>
2024-06-21 03:00:40 +0200

    dashboard and  recycle services

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
-  theme: {
-    extend: {
-      colors:{
-        "customBlue":"#7FDBC5",
-        "customGray":"#D9D9D9"
-      }
-    },
+import colors from 'tailwindcss/colors'
+export const content = ["./src/**/*.{html,js}", "./*.html"]
+export const theme = {
+  extend: {
+    colors: {
+      "customBlue": "#7FDBC5",
+      "customGray": "#D9D9D9",
+      newRed: "#b5071c",
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      stone: colors.warmGray,
+      sky: colors.lightBlue,
+      neutral: colors.trueGray,
+      gray: colors.coolGray,
+      slate: colors.blueGray,
+      red:colors.red
+    }
   },
-  plugins: [],
 }
+export const plugins = []
 
